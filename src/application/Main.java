0bcda5edeb34f083a677cd4625d69b4ff7fcfda6
Alexandre Souza <allexandre.souza2307@gmail.com>
2023-05-11 00:50:35 -0300

    implementação da classe 'Seller'

--- src/application/Main.java
+++ src/application/Main.java
@@ -1,10 +1,14 @@
 package application;
 
 import model.entities.Department;
+import model.entities.Seller;
+
+import java.util.Date;
 
 public class Main {
     public static void main(String[] args) {
         Department obj = new Department(1,"books");
-        System.out.println(obj);
+        Seller seller = new Seller(21,"bob","bob@email.com",new Date(),2100.00, obj);
+        System.out.println(seller);
     }
 }
