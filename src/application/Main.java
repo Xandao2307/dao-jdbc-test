35229c8b6faa759179ee37cdf4d229f4e612d708
Alexandre Souza <allexandre.souza2307@gmail.com>
2023-05-15 02:51:59 -0300

    Implemetação do findById

--- src/application/Main.java
+++ src/application/Main.java
@@ -10,10 +10,8 @@ import java.util.Date;
 
 public class Main {
     public static void main(String[] args) {
-        Department obj = new Department(1,"books");
-        Seller seller = new Seller(21,"bob","bob@email.com",new Date(),2100.00, obj);
+        SellerDao sellerDao= DaoFactory.createSellerDao();
+        Seller seller = sellerDao.findById(3);
         System.out.println(seller);
-        SellerDao sellerDao = DaoFactory.createSellerDao();
-        DepartmentDao departmentDao = DaoFactory.createDepartmentDao();
     }
 }
